c4634e00adb5101b06eb5542fb31fe0fc4c700af
Gavin Uberti <gavin.uberti@gmail.com>
2022-12-28 16:05:59 -0800

    New website version

--- docs/_downloads/7f1d3d1b878694c201c614c807cdebc8/from_tensorflow.py
+++ docs/_downloads/7f1d3d1b878694c201c614c807cdebc8/from_tensorflow.py
@@ -30,6 +30,7 @@ Please refer to https://www.tensorflow.org/install
 """
 
 # sphinx_gallery_start_ignore
+# sphinx_gallery_requires_cuda = True
 from tvm import testing
 
 testing.utils.install_request_hook(depth=3)
